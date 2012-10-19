9165decad69d33faf3ee1977c5f8058b7c8cc347
Maciek <mbassara@gmail.com>
2012-10-20 00:36:31 +0200

    Dodana obsługa plików .tsp. Na razie tylko jeden typ w którym podane są współrzędne punktów (taki nas będzie interesował jeśli chcemy to jakoś graficznie prezentować). Jest możliwość serializacji do pliku, deserializacja danych z pliku do obiektu TSPData i generowanie przykładowych danych.

---
+++ TSP-Solver/src/pl/edu/agh/iet/tsp_solver/Hello.java
@@ -0,0 +1,26 @@
+package pl.edu.agh.iet.tsp_solver;
+
+import java.io.File;
+import java.io.IOException;
+
+import pl.edu.agh.iet.tsp_solver.data.TSPDataSerialization;
+import pl.edu.agh.iet.tsp_solver.data.TSPData;
+
+public class Hello {
+
+	public static void main(String[] args) throws IOException {
+
+		File sampleDir = new File("./sample_data");
+
+		if (sampleDir.isDirectory()) {
+			for (File file : sampleDir.listFiles()) {
+				TSPData data = TSPDataSerialization.deserialize(file);
+				System.out.println(data + "\n");
+			}
+		}
+		
+		TSPData sample = TSPDataSerialization.generateData("sample", "przyk³adowe wygenerowane dane wejœciowe dla naszego programu", 69, 1500);
+		TSPDataSerialization.serialize(sample, new File("./sample_data/sample.tsp"));
+	}
+
+}
---
+++ TSP-Solver/src/pl/edu/agh/iet/tsp_solver/data/TSPData.java
@@ -0,0 +1,53 @@
+package pl.edu.agh.iet.tsp_solver.data;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TSPData {
+
+	private String name;
+	private String comment;
+	private int dimension;
+	private ArrayList<Field> nodes;
+
+	public TSPData(String name, String comment, int dimension,
+			Collection<Field> nodes) {
+		this.name = name;
+		this.comment = comment;
+		this.dimension = dimension;
+		this.nodes = new ArrayList<Field>(nodes);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public int getDimension() {
+		return dimension;
+	}
+
+	public Collection<Field> getNodes() {
+		return nodes;
+	}
+
+	@Override
+	public String toString() {
+		String result = "TSV Data Object:";
+		if(name != null) result += "\n\tname:\t\t" + name;
+		if(comment != null) result += "\n\tcomment:\t" + comment;
+		if(dimension != -1) result += "\n\tdimmen:\t\t" + dimension;
+		if(nodes != null && nodes.size() > 0){
+			result += "\n\tpoints:\t\t";
+			if (nodes != null)
+				for (Field field : nodes)
+					result += field + " ";
+			result.substring(0, result.length() - 2);
+		}
+
+		return result;
+	}
+
+}
